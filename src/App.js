3881280e0e6a79c01d3eb06c2b484121ae012c97
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-04-12 16:52:23 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useEffect, useState } from "react";
+import Display from "./components/display/Display";
+import Gameover from "./components/gameover/Gameover";
+import {CircleProgress} from 'react-gradient-progress';
+import  './style.css'
+
+
+
 
 function App() {
+
+  
+  // var treino = 100;
+  // var passeio = 100;
+  // var remedio = 100;
+
+  const [fome, setfome] = useState(100);
+  const [treino, setTreino] = useState(100)
+  const [passeio, setPasseio] = useState(100)
+  const [remedio, setRemedio] = useState(100)
+
+  const total = fome + treino + passeio + remedio
+
+ function AddFome () {
+   setfome(fome + 10)
+ }
+
+ function AddTreino () {
+  setTreino(treino + 10)
+}
+
+function AddPasseio () {
+  setPasseio(passeio + 10)
+}
+
+function AddRemedio () {
+  
+setRemedio(remedio + 10)
+  
+}
+console.log(remedio)
+
+
+  useEffect(()=> {
+    if (fome !== 0) {
+      setTimeout(() => {
+      setfome(fome -3)
+    }, 80000);
+    }
+    
+  },[fome])
+
+  useEffect(()=> {
+    if (treino !== 0) {
+      setTimeout(() => {
+      setTreino(treino -2)
+    }, 50000);
+    }
+    
+  },[treino])
+     
+  useEffect(()=> {
+    if (passeio !== 0) {
+      setTimeout(() => {
+      setPasseio(passeio -3)
+    }, 70000);
+    }
+    
+  },[passeio])
+
+  
+    if (remedio !== 0) {
+      setTimeout(() => {
+      setRemedio(remedio -5)
+      
+    }, 10000);
+    }
+    
+  
+  
+  
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="ProgressContainer">
+        <CircleProgress id='fome 'percentage={fome} strokeWidth={8} width={100} />
+        <CircleProgress id='treino' percentage={treino} strokeWidth={8} width={100} />
+        <CircleProgress percentage={passeio} strokeWidth={8} width={100} />
+        <CircleProgress percentage={remedio} strokeWidth={8} width={100} />
+      </div>
+      {total !== 0 ? <Display /> : <Gameover /> }
+      <nav>
+                <button onClick={AddFome} >fome</button>
+                <button onClick={AddTreino}>treino</button>
+                <button onClick={AddPasseio}>passeio</button>
+                <button onClick={AddRemedio}>remedio</button>
+            </nav>
+      
+        
     </div>
   );
 }
