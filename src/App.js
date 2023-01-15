e4c0457358e8dbb1b248d36b97102b04ab062796
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2023-01-15 19:42:37 -0300

    comit4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { Menu } from './components/Menu';
-import { Header } from './components/Header';
-import { Chanel } from './components/Chanel';
-import { Box } from '@material-ui/core';
-import '../src/global.css'
-
-
-
-
-
-
-
-
+import { Menu } from "./components/Menu";
+import { Header } from "./components/Header";
+import { Chanel } from "./components/Chanel";
+import { Box } from "@material-ui/core";
+import "../src/global.css";
 
 function App() {
-
-
-
- 
   return (
- 
     <Box>
       <Header />
-      <Box style={{display:'flex'}}>
-      <Menu />
-      {/* <Embed /> */}
-      <Chanel />
+      <Box style={{ display: "flex" }}>
+        <Menu />
+        {/* <Embed /> */}
+        <Chanel />
       </Box>
     </Box>
   );
